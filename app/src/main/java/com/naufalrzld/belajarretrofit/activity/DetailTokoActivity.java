a8f2de7020000f11aec7f8e319a183a50128552a
Naufal <naufalrzld@gmail.com>
2018-01-30 17:06:08 +0700

    Add Detail Toko Activity

---
+++ app/src/main/java/com/naufalrzld/belajarretrofit/activity/DetailTokoActivity.java
@@ -0,0 +1,15 @@
+package com.naufalrzld.belajarretrofit.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.naufalrzld.belajarretrofit.R;
+
+public class DetailTokoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_toko);
+    }
+}
